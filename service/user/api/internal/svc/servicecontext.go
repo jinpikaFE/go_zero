bea67c4fde5f56ba3ec8c1e6cd9b0585da29e8b6
jinxin <jinxin@hsmap.com>
2022-11-02 11:21:47 +0800

    chore: init

---
+++ service/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,20 @@
+package svc
+
+import (
+	"github.com/zeromicro/go-zero/zrpc"
+	"go_zero/service/user/api/internal/config"
+	"go_zero/service/user/rpc/userclient"
+)
+
+type ServiceContext struct {
+	Config config.Config
+
+	UserRpc userclient.User
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config:  c,
+		UserRpc: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+	}
+}
